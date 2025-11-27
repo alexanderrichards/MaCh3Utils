da9595e69c69fdcd6f27f9608c94cf7eae78f1f7
laurilaatu <laurilaatu@users.noreply.github.com>
2025-11-27 08:28:24 +0000

    Add CPU and FPGA usage flags to build script

--- tutorial_build.sh
+++ tutorial_build.sh
@@ -4,24 +4,30 @@ FPGA_DEVICE=Agilex7
 COMPILE_EMULATOR=FALSE
 COMPILE_REPORT=FALSE
 COMPILE_HARDWARE=FALSE
+USE_CPU=FALSE
+USE_FPGA=TRUE
 MaCh3_Branch=intel_icpx
 
 case $1 in
     emulator)
-	COMPILE_EMULATOR=TRUE
-	;;
+        COMPILE_EMULATOR=TRUE
+        ;;
     report)
         FPGA_DEVICE=/opt/oneapi-asp/ia840f/:ofs_ia840f_usm
-	COMPILE_REPORT=TRUE
-	;;
+        COMPILE_REPORT=TRUE
+        ;;
     hardware)
         FPGA_DEVICE=/opt/oneapi-asp/ia840f/:ofs_ia840f_usm
-	COMPILE_HARDWARE=TRUE
-	;;
+        COMPILE_HARDWARE=TRUE
+        ;;
+    cpu)
+        USE_CPU=TRUE
+        USE_FPGA=FALSE
+        ;;
     *)
-	echo "Missing/unknown parameter, choose from [emulator/report/hardware]"
-	exit 1
-	;;
+        echo "Missing/unknown parameter, choose from [cpu/emulator/report/hardware]"
+        exit 1
+        ;;
 esac
 
 echo "Build Flags:"
@@ -29,6 +35,8 @@ echo "  FPGA_DEVICE: ${FPGA_DEVICE}"
 echo "  COMPILE_EMULATOR: ${COMPILE_EMULATOR}"
 echo "  COMPILE_REPORT: ${COMPILE_REPORT}"
 echo "  COMPILE_HARDWARE: ${COMPILE_HARDWARE}"
+echo "  USE_CPU: ${USE_CPU}"
+echo "  USE_FPGA: ${USE_FPGA}"
 
 export QUARTUS_ROOTDIR_OVERRIDE=/opt/intelFPGA_pro/23.1.0/quartus/
 export LM_LICENSE_FILE=5280@licsrv00.hep.ph.ic.ac.uk
@@ -53,8 +61,8 @@ set -x
     -DCMAKE_AR:FILEPATH=/usr/bin/gcc-ar \
     -DCMAKE_CC_COMPILER:FILEPATH=/opt/intel/oneapi/compiler/2025.0/bin/icx \
     -DCMAKE_CXX_COMPILER:FILEPATH=/opt/intel/oneapi/compiler/2025.0/bin/icpx \
-    -DUSE_CPU:BOOL=FALSE \
-    -DUSE_FPGA:BOOL=TRUE \
+    -DUSE_CPU:BOOL=${USE_CPU} \
+    -DUSE_FPGA:BOOL=${USE_FPGA} \
     -DMaCh3_Branch:STRING=${MaCh3_Branch} \
     -DFPGA_DEVICE=${FPGA_DEVICE} \
     -DCOMPILE_EMULATOR:BOOL=${COMPILE_EMULATOR} \
